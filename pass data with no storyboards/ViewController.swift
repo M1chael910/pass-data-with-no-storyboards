2c32115c03f0c62785dde1b670808e0f0ae444e5
Michael Murphy <mpmurphy1432@gmail.com>
2018-12-01 16:37:19 -0600

    Initial Commit

---
+++ pass data with no storyboards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pass data with no storyboards
+//
+//  Created by Michael  Murphy on 12/1/18.
+//  Copyright © 2018 Michael  Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
